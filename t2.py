cfd698b9bfc239482b007c64f89c023195e39d7b
EkaterinaKat <100533583+qweyhb@users.noreply.github.com>
2023-05-15 12:58:34 +0300

    Update t2.py

--- t2.py
+++ t2.py
@@ -11,7 +11,7 @@ username.send_keys('ekaterina_katkova0707@mail.ru')
 time.sleep(1)
 
 password=browser.find_element(by=By.NAME, value='password')
-password.send_keys('QXCF637dg')
+password.send_keys('QWEgfjs73d')
 time.sleep(1)
 
 button=browser.find_element(by=By.XPATH, value='//*[@id="auth-view-page"]/button[2]')
@@ -21,6 +21,13 @@ time.sleep(20)
 button=browser.find_element(by=By.XPATH, value='//*[@id="react-page"]/div/div/div/div[1]/div[3]/div[6]/div[2]/a[1]')
 button.click()
 time.sleep(5)
+
+try:
+    assert 'Figma' in browser.title
+    assert "Untitled" in browser.page_source
+    print('Тест прошел удачно')
+except Exception as err:
+    print('Тест прошел неудачно')
 
 #Закрываем браузер
 browser.close()
